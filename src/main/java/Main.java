40c668b0a23f0c72007a8c574bdc86fd9dd0fc22
Juan Antezana Adrian <juan.antezana.adrian@gmail.com>
2022-08-01 21:15:10 -0400

    Initial structure

---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Main class
+ *
+ * @author jantezana
+ * @since 2022/08/01
+ */
+public class Main {
+
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        logger.info("[JobRunr - examples]");
+    }
+}
