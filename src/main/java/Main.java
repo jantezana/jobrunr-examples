ff0ea7f66600fca891949cc8681bb1876f81a9ba
Juan Antezana Adrian <juan.antezana.adrian@gmail.com>
2022-08-01 22:34:06 -0400

    Created a BackgroundJob.enqueue

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,6 @@
+import org.jobrunr.configuration.JobRunr;
+import org.jobrunr.scheduling.BackgroundJob;
+import org.jobrunr.storage.InMemoryStorageProvider;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,7 +14,18 @@ public class Main {
 
     private static final Logger logger = LoggerFactory.getLogger(Main.class);
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         logger.info("[JobRunr - examples]");
+
+        JobRunr.configure()
+               .useStorageProvider(new InMemoryStorageProvider())
+               .useBackgroundJobServer()
+               .useDashboard()
+               .initialize();
+
+        BackgroundJob.enqueue(() -> System.out.println("BackgroundJob.enqueue"));
+
+        // keep dashboard running by blocking the main thread
+        Thread.currentThread().join();
     }
 }
